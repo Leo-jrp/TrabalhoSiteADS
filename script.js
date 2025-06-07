93238bb8dd4c9be4496b9ae9b6daf32f5a8ad43e
Leo-jrp <152932030+Leo-jrp@users.noreply.github.com>
2025-06-07 12:27:55 -0300

    Teste1

---
+++ script.js
@@ -0,0 +1,5 @@
+function mostrarMensagem() {
+    const div = document.querySelector("#mensagem");
+    div.innerHTML = "“Nenhuma árvore pode crescer até o céu sem que suas raízes desçam até o inferno.”";
+    div.classList.add("mostrar");
+}
